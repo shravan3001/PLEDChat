61f6820ca42b48dcbec15d03c2da27c534a60d99
Shravan Kumar <shravan.nitwcse@gmail.com>
2024-11-15 02:47:28 +0530

    Basic React Frontend and Home page created using TypeScript

---
+++ reactchat/src/App.tsx
@@ -0,0 +1,17 @@
+import Home from "./pages/Home";
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
+  )
+)
+
+const App: React.FC = () => {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+export default App
